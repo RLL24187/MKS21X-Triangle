7206f11525bb5b2576eb3464bbf38a00ef925c68
rleongPd5 <rleong00@stuy.edu>
2018-10-10 02:11:30 -0400

    Added getters

--- Point.java
+++ Point.java
@@ -2,8 +2,16 @@ public class Point{
   //Instance variables
   private double x;
   private double y;
+  //Constructor for making a point
   public Point(double xcor, double ycor){
     x = xcor;
     y = ycor;
+  }
+  //Get variables
+  public double getX(){
+    return this.x;
+  }
+  public double getY(){
+    return this.y;
   }
 }
