a89c35af9368ee857d1f341d8f875f16a1c34888
rleongPd5 <rleong00@stuy.edu>
2018-10-10 01:59:14 -0400

    Removed main, added instance variables and Triangle constructor.

--- Triangle.java
+++ Triangle.java
@@ -1,6 +1,12 @@
 public class Triangle{
-  public Triangle(Point, Point, Point)
-  public static void main(String[] args){
-
+  //Instance variables
+  private Point v1;
+  private Point v2;
+  private Point v3;
+  //Constructor to make a triangle
+  public Triangle(Point A, Point B, Point C){
+    v1 = A;
+    v2 = B;
+    v3 = C;
   }
 }
