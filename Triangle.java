ec9eec18ac4eeb08a040b7779982adec2f7990b9
rleongPd5 <rleong00@stuy.edu>
2018-10-10 02:17:29 -0400

    Added Triangle constructor using doubles, adjusted Triangle constructor using points.

--- Triangle.java
+++ Triangle.java
@@ -3,10 +3,19 @@ public class Triangle{
   private Point v1;
   private Point v2;
   private Point v3;
-  //Constructor to make a triangle
+  //Constructor to make a triangle given points
   public Triangle(Point A, Point B, Point C){
-    v1 = A;
-    v2 = B;
-    v3 = C;
+    v1 = (A.x, A.y);
+    v2 = (B.x, B.y;
+    v3 = (C.x, C.y);
+  }
+  //Constructor to make a triangle given doubles
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
+    v1.x = x1;
+    v1.y = y1;
+    v2.x = x2;
+    v2.y = y2;
+    v3.x = x3;
+    v3.y = y3;
   }
 }
